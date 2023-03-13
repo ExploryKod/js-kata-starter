acfceaa4f7f9a50b905ec00a3ea7a778f28a2b57
Amaury Franssen <amauryfranssen@gmail.com>
2023-03-13 12:19:16 +0100

    refactoring

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,111 +1,105 @@
-import computeManhattanDistance, { Point } from ".";
-
-test("The distance between a point and itself should equal zero", () => {
+import fizzBuzz from "./index";
+// Chercher le cas par défaut - ici c'est 1 (et autant que possible on cherche à renvoyer tj le même type, donc en string ici)
+test("the digit is equal to 1", () => {
   // GIVEN
-  const a: Point = {
-    x: 0,
-    y: 0,
-  };
+  const numb: number = 1;
 
   // WHEN
-  const actual = computeManhattanDistance(a, a);
+  const actual = fizzBuzz(numb);
 
   // THEN
-  const expected: number = 0;
+  const expected: string = '1';
   expect(actual).toEqual(expected);
 });
 
-test("Compute the distance on the x axis", () => {
+test("the digit is equal to 2", () => {
   // GIVEN
-  const a: Point = {
-    x: 1,
-    y: 0,
-  };
-  const b: Point = {
-    x: 2,
-    y: 0,
-  };
+  const numb: number = 2;
 
   // WHEN
-  const actual = computeManhattanDistance(a, b);
+  const actual = fizzBuzz(numb);
 
   // THEN
-  const expected: number = 1;
+  const expected: string = '2';
   expect(actual).toEqual(expected);
 });
 
-test("A distance must be a positive number", () => {
+test("the digit is a multiple of 3 so we have Fizz", () => {
   // GIVEN
-  const a: Point = {
-    x: 0,
-    y: 0,
-  };
-  const b: Point = {
-    x: -1,
-    y: 0,
-  };
+  const numb: number = 3;
 
   // WHEN
-  const actual = computeManhattanDistance(a, b);
+  const actual = fizzBuzz(numb);
 
   // THEN
-  const expected: number = 1;
+  const expected: string = 'Fizz';
   expect(actual).toEqual(expected);
 });
 
-test("Compute the distance on the y axis", () => {
+// triangulation: je garde le même algo et le même test avec une valeur différente qui respecte la logique précèdente (pas des specs) (multiple de 3)
+test("the digit is a multiple of 3 so we have Fizz", () => {
   // GIVEN
-  const a: Point = {
-    x: 0,
-    y: 0,
-  };
-  const b: Point = {
-    x: 0,
-    y: 1,
-  };
+  const numb: number = 6;
 
   // WHEN
-  const actual = computeManhattanDistance(a, b);
+  const actual = fizzBuzz(numb);
 
   // THEN
-  const expected: number = 1;
+  const expected: string = 'Fizz';
   expect(actual).toEqual(expected);
 });
 
-test("Compute the distance on the y axis", () => {
+// D'abord Red => ensuite green => enfin refactor
+test("the digit is a multiple of 5 so we have Buzz", () => {
   // GIVEN
-  const a: Point = {
-    x: 0,
-    y: 3,
-  };
-  const b: Point = {
-    x: 0,
-    y: 1,
-  };
+  const numb: number = 5;
 
   // WHEN
-  const actual = computeManhattanDistance(a, b);
+  const actual = fizzBuzz(numb);
 
   // THEN
-  const expected: number = 2;
+  const expected: string = 'Buzz';
   expect(actual).toEqual(expected);
 });
 
-test("Compute the manhattan distance (both x and y dimensions)", () => {
+// Triangulation
+test("the digit is a multiple of 5 so we have Buzz", () => {
+
   // GIVEN
-  const a: Point = {
-    x: 0,
-    y: 0,
-  };
-  const b: Point = {
-    x: 1,
-    y: 1,
-  };
+  const numb: number = 10;
 
   // WHEN
-  const actual = computeManhattanDistance(a, b);
+  const actual = fizzBuzz(numb);
 
   // THEN
-  const expected: number = 2;
+  const expected: string = 'Buzz';
+  expect(actual).toEqual(expected);
+});
+
+
+test("the digit is a multiple of 5 and 3 so we have FizzBuzz", () => {
+
+  // GIVEN
+  const numb: number = 15;
+
+  // WHEN
+  const actual = fizzBuzz(numb);
+
+  // THEN
+  const expected: string = 'FizzBuzz';
+  expect(actual).toEqual(expected);
+});
+
+// Triangulation - on change la valeur de numb
+test("the digit is a multiple of 5 and 3 so we have FizzBuzz", () => {
+
+  // GIVEN
+  const numb: number = 30;
+
+  // WHEN
+  const actual = fizzBuzz(numb);
+
+  // THEN
+  const expected: string = 'FizzBuzz';
   expect(actual).toEqual(expected);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,25 @@
-export type Point = {
-    x: number;
-    y: number;
-  };
-  
-  const computeOneDimensionDistance = (a: number, b: number) => {
-    return Math.abs(b - a);
-  };
-  
-  const computeManhattanDistance = (a: Point, b: Point): number => {
-    return (
-      computeOneDimensionDistance(b.x, a.x) + //
-      computeOneDimensionDistance(b.y, a.y)
-    );
-  };
-  
-  export default computeManhattanDistance;
+const isMultipleOf = (divider: number, n: number): boolean => {
+    return n % divider === 0;
+};
+const fizzBuzz = (num: number): string => {
+    let result = "";
+    if (num % 3 === 0) {
+        result += 'Fizz';
+    }
+    if (num % 5 === 0 && num % 3 === 0) {
+        result += result;
+    }
+    if (num % 5 === 0) {
+        result += 'Buzz';
+    }
+    return num.toString()
+};
+
+export default fizzBuzz;
+
+/** 
+ * Phase de Refactor :
+ * ne retourner ici que le type string (on change donc aussi les test)
+ * naming : result et non str etc...
+ * On donne de l'intention avec une fonction isMultipleOf3 (et le SRP)
+ **/ 
